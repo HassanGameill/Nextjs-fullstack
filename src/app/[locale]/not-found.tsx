faedfe35bdbd96c1460bff1d5d17ef57fbf24976
HassanGameill <hasanaboalion@gmail.com>
2024-09-30 21:45:42 +0300

    update feature

--- src/app/[locale]/not-found.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const NotfoundPage = () => {
-  return (
-    <div>not-found</div>
-  )
-}
-
-export default NotfoundPage
--- src/i18n.ts
+++ src/i18n.ts
@@ -1,13 +1,17 @@
-import {notFound} from 'next/navigation';
-import {getRequestConfig} from 'next-intl/server';
-import {locales} from "./config"
- 
- 
-export default getRequestConfig(async ({locale}) => {
-  // Validate that the incoming `locale` parameter is valid
-  if (!locales.includes(locale as any)) notFound();
- 
+import { notFound } from 'next/navigation';
+import { getRequestConfig } from 'next-intl/server';
+import { locales } from './config';
+
+// Infer the type from the locales array
+type Locale = typeof locales[number]; // "en" | "ar"
+
+export default getRequestConfig(async ({ locale }) => {
+  // Type assertion to ensure `locale` matches the valid types
+  if (!locales.includes(locale as Locale)) {
+    notFound();
+  }
+
   return {
     messages: (await import(`../messages/${locale}.json`)).default,
   };
-});                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+});
